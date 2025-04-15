252d1973d5e32262b50f289e9a6305fdc0e8e3d5
Ilyameraii <ilia.bobrow2006@gmail.com>
2025-04-15 14:59:28 +0300

    Infection

---
+++ BIA_Task_8/MainForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace BIA_Task_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel = new System.Windows.Forms.Panel();
+            this.numericUpDownSize = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownInterval = new System.Windows.Forms.NumericUpDown();
+            this.labelSize = new System.Windows.Forms.Label();
+            this.labelInterval = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.numericUpDownInfection = new System.Windows.Forms.NumericUpDown();
+            this.labelInfectiton = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInfection)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(100, 74);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(300, 300);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // numericUpDownSize
+            // 
+            this.numericUpDownSize.Location = new System.Drawing.Point(100, 48);
+            this.numericUpDownSize.Maximum = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            this.numericUpDownSize.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownSize.Name = "numericUpDownSize";
+            this.numericUpDownSize.Size = new System.Drawing.Size(67, 20);
+            this.numericUpDownSize.TabIndex = 1;
+            this.numericUpDownSize.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownSize.ValueChanged += new System.EventHandler(this.numericUpDownSize_ValueChanged);
+            // 
+            // numericUpDownInterval
+            // 
+            this.numericUpDownInterval.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.Location = new System.Drawing.Point(204, 48);
+            this.numericUpDownInterval.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.Name = "numericUpDownInterval";
+            this.numericUpDownInterval.Size = new System.Drawing.Size(64, 20);
+            this.numericUpDownInterval.TabIndex = 2;
+            this.numericUpDownInterval.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.ValueChanged += new System.EventHandler(this.numericUpDownInterval_ValueChanged);
+            // 
+            // labelSize
+            // 
+            this.labelSize.AutoSize = true;
+            this.labelSize.Location = new System.Drawing.Point(85, 32);
+            this.labelSize.Name = "labelSize";
+            this.labelSize.Size = new System.Drawing.Size(94, 13);
+            this.labelSize.TabIndex = 3;
+            this.labelSize.Text = "Размер матрицы";
+            // 
+            // labelInterval
+            // 
+            this.labelInterval.AutoSize = true;
+            this.labelInterval.Location = new System.Drawing.Point(185, 32);
+            this.labelInterval.Name = "labelInterval";
+            this.labelInterval.Size = new System.Drawing.Size(103, 13);
+            this.labelInterval.TabIndex = 4;
+            this.labelInterval.Text = "Интервал времени";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(104, 407);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(320, 407);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 6;
+            this.buttonStop.Text = "Стоп";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTimer.Location = new System.Drawing.Point(221, 377);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(54, 20);
+            this.labelTimer.TabIndex = 8;
+            this.labelTimer.Text = "00:00";
+            // 
+            // numericUpDownInfection
+            // 
+            this.numericUpDownInfection.DecimalPlaces = 2;
+            this.numericUpDownInfection.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDownInfection.Location = new System.Drawing.Point(320, 48);
+            this.numericUpDownInfection.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownInfection.Name = "numericUpDownInfection";
+            this.numericUpDownInfection.Size = new System.Drawing.Size(67, 20);
+            this.numericUpDownInfection.TabIndex = 9;
+            this.numericUpDownInfection.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            // 
+            // labelInfectiton
+            // 
+            this.labelInfectiton.Location = new System.Drawing.Point(294, 20);
+            this.labelInfectiton.Name = "labelInfectiton";
+            this.labelInfectiton.Size = new System.Drawing.Size(126, 39);
+            this.labelInfectiton.TabIndex = 10;
+            this.labelInfectiton.Text = "Шанс заражения клетки с иммунитетом";
+            this.labelInfectiton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 473);
+            this.Controls.Add(this.numericUpDownInfection);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelInterval);
+            this.Controls.Add(this.labelSize);
+            this.Controls.Add(this.numericUpDownInterval);
+            this.Controls.Add(this.numericUpDownSize);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.labelInfectiton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Форма";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInfection)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.NumericUpDown numericUpDownSize;
+        private System.Windows.Forms.NumericUpDown numericUpDownInterval;
+        private System.Windows.Forms.Label labelSize;
+        private System.Windows.Forms.Label labelInterval;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.NumericUpDown numericUpDownInfection;
+        private System.Windows.Forms.Label labelInfectiton;
+    }
+}
+
---
+++ BIA_Task_8/MainForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BIA_Task_8
+{
+    public partial class Form1 : Form
+    {
+        private int N = 5;
+        private int[,] cell = new int[5, 5];
+        private const int Healthy = 0; // Здоровая клетка
+        private const int Infected = 1; // Зараженная клетка
+        private const int Immune = 8; // Невосприимчивая клетка
+        private int a = 0; // цифра 4 для таймера
+        private int b = 0; // цифра 3 для таймера
+        private int c = 0; // цифра 2 для таймера
+        private int d = 0; // цифра 1 для таймера
+        Random rand = new Random(); //рандом 
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            int N = Convert.ToInt32(numericUpDownSize.Value); // Размер матрицы N x N
+            int PixelSize = panel.Width / N; // Размер одного "пикселя"
+            // создаем матрицу из Button
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Button pixel = new Button();
+                    pixel.Name = $"pixel{i}{j}";
+                    pixel.Size = new Size(PixelSize, PixelSize);
+                    pixel.Location = new Point(j * PixelSize, i * PixelSize);
+                    pixel.BackColor = Color.White;
+                    pixel.FlatStyle = FlatStyle.Flat;
+                    pixel.FlatAppearance.BorderSize = 0;
+                    pixel.Click += Pixel_Click; // Обработка клика
+                    if (i == N / 2 && j == N / 2)
+                    {
+                        pixel.BackColor = Color.Black;
+                    }
+                    panel.Controls.Add(pixel);
+
+                }
+            }
+
+        }
+        // Обработка клика по пикселю
+        private void Pixel_Click(object sender, EventArgs e)
+        {
+            Button pixel = (Button)sender;
+            if (pixel.BackColor == Color.White)
+            {
+
+                pixel.BackColor = Color.Black; // Меняем цвет пикселя
+            }
+            else if (pixel.BackColor == Color.Black)
+            {
+
+                pixel.BackColor = Color.White; // Меняем цвет пикселя
+            }
+
+        }
+
+        private void numericUpDownSize_ValueChanged(object sender, EventArgs e)
+        {
+            N = Convert.ToInt16(numericUpDownSize.Value); // Размер матрицы N x N
+            int PixelSize = panel.Width / N; // Размер одного "пикселя"
+
+            panel.Controls.Clear(); // Очищаем панель перед созданием новых кнопок
+            // пересоздаем матрицу из Button
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Button pixel = new Button();
+                    pixel.Name = $"pixel{i}{j}";
+                    pixel.Size = new Size(PixelSize, PixelSize);
+                    pixel.Location = new Point(j * PixelSize, i * PixelSize);
+                    pixel.BackColor = Color.White;
+                    pixel.FlatStyle = FlatStyle.Flat;
+                    pixel.FlatAppearance.BorderSize = 0;
+                    pixel.Click += Pixel_Click; // Обработка клика
+                    if (i == N / 2 && j == N / 2)
+                    {
+                        pixel.BackColor = Color.Black;
+                    }
+                    panel.Controls.Add(pixel);
+                }
+            }
+            // пересоздаем матрицу значений
+            cell = new int[N, N];
+        }
+
+        private void numericUpDownInterval_ValueChanged(object sender, EventArgs e)
+        {
+            timer.Interval = Convert.ToInt32(numericUpDownInterval.Value);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+        }
+        private void UpdateCellStates()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (cell[i, j] >= Infected && cell[i, j] < Immune)
+                    {
+                        cell[i, j]++;
+                        infection(i, j);
+                    }
+                    else if (cell[i, j] >= Immune)
+                    {
+                        // Клетка невосприимчива
+                        cell[i, j]++;
+                        if (cell[i, j] > Immune + 4)
+                        {
+                            // Клетка выздоравливает
+                            cell[i, j] = Healthy;
+                        }
+                    }
+                }
+            }
+        }
+        private void UpdateCellColors()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    string pixelName = $"pixel{i}{j}";
+                    Button pixel = panel.Controls.Find(pixelName, true).FirstOrDefault() as Button;
+                    if (cell[i, j] == Healthy)
+                    {
+                        pixel.BackColor = Color.White;
+                    }
+                    else if (cell[i, j] >= Infected && cell[i, j] < Immune)
+                    {
+                        pixel.BackColor = Color.Black;
+                    }
+                    else if (cell[i, j] >= Immune)
+                    {
+                        pixel.BackColor = Color.LawnGreen;
+                    }
+                }
+            }
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // секундомер
+            a++;
+            if (a > 9)
+            {
+                a = 0;
+                b++;
+            }
+            if (b > 5)
+            {
+                b = 0;
+                c++;
+            }
+            if (c > 9)
+            {
+                c = 0;
+                d++;
+            }
+            if (d > 5)
+            {
+                d = 0;
+            }
+            labelTimer.Text = $"{d}{c}:{b}{a}";
+
+            // записываем единицу в матрицу для закрашенных черных элементов
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    string pixelName = $"pixel{i}{j}";
+                    Button pixel = panel.Controls.Find(pixelName, true).FirstOrDefault() as Button;
+                    if (pixel.BackColor == Color.Black && cell[i, j] < Infected)
+                    {
+                        cell[i, j] = 1;
+                    }
+                }
+            }
+            UpdateCellStates();
+            UpdateCellColors();
+        }
+        private void infection(int i, int j)
+        {
+            for (int choose = 0; choose < 4; choose++)
+            {
+                int chance = rand.Next(2);
+                if (chance == 0)
+                {
+                    switch (choose)
+                    {
+                        case 0:
+                            if (i - 1 >= 0)
+                            {
+                                if (cell[i - 1, j] == Healthy|| (cell[i - 1, j] >=Immune && rand.NextDouble() < Convert.ToDouble(numericUpDownInfection.Value)))
+                                {
+                                    cell[i - 1, j] = Infected;
+                                }
+                            }
+                            break;
+                        case 1:
+                            if (j - 1 >= 0)
+                            {
+                                if (cell[i, j - 1] == Healthy || (cell[i, j - 1] >= Immune && rand.NextDouble() < Convert.ToDouble(numericUpDownInfection.Value)))
+                                {
+                                    cell[i, j - 1] = Infected;
+                                }
+                            }
+                            break;
+                        case 2:
+                            if (i + 1 < N)
+                            {
+                                if (cell[i + 1, j] == Healthy || (cell[i + 1, j] >= Immune && rand.NextDouble() < Convert.ToDouble(numericUpDownInfection.Value)))
+                                {
+                                    cell[i + 1, j] = Infected;
+                                }
+                            }
+                            break;
+                        case 3:
+                            if (j + 1 < N)
+                            {
+                                if (cell[i, j + 1] == Healthy || (cell[i, j + 1] >= Immune && rand.NextDouble() < Convert.ToDouble(numericUpDownInfection.Value)))
+                                {
+                                    cell[i, j + 1] = Infected;
+                                }
+                            }
+                            break;
+                    }
+                }
+            }
+
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
